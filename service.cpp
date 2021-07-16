a40d16d5a5b34884784d974aafb1644227a1fb0a
MADMechaniculus <rihanna271298@gmail.com>
2021-07-16 14:08:26 +0300

    Initial commit

---
+++ service.cpp
@@ -0,0 +1,164 @@
+//
+// Created by madded on 13.07.2021.
+//
+
+#include "../headers/service/service.h"
+#include <ctime>
+
+// Получение смещения
+time_t custom_time::get_offset_from_zero() {
+    struct tm * tm_ptr;
+    tm_ptr = std::localtime(new time_t(0));
+    return mktime(tm_ptr);
+}
+
+// Преобразование локального времени к единому по UTC
+time_t custom_time::local_time_to_UTC(time_t local_t) {
+    return local_t - custom_time::get_offset_from_zero();
+}
+
+// Перевод из формата UTC в формат используемый на ISS
+time_t custom_time::from_UTC_to_ISS(time_t utc_time) {
+    time_t ret = utc_time - custom_time::y06011998;
+    return ret;
+}
+
+// Перевод из формата ISS в формат UTC
+time_t custom_time::from_ISS_to_UTC(time_t iss_time) {
+    time_t ret = iss_time + custom_time::y06011998;
+    return ret;
+}
+
+template<class T>
+async_containers::async_vector_wrapper<T>::async_vector_wrapper() {
+    this->data_ptr = vec.data();
+}
+
+template<class T>
+void async_containers::async_vector_wrapper<T>::lock(async_vector_wrapper<T> &inst) {
+    inst.mx.lock();
+}
+
+template<class T>
+void async_containers::async_vector_wrapper<T>::unlock(async_vector_wrapper<T> &inst) {
+    inst.mx.unlock();
+}
+
+template<class T>
+T *async_containers::async_vector_wrapper<T>::get_data_ptr(void) {
+    return vec.data();
+}
+
+template<class T>
+void async_containers::async_vector_wrapper<T>::get_size(size_t &out_s) {
+    BOOST_LOCK(mx);
+    out_s = vec.size();
+}
+
+template<class T>
+void async_containers::async_vector_wrapper<T>::clear() {
+    BOOST_LOCK(mx);
+    vec.clear();
+}
+
+template<class T>
+void async_containers::async_vector_wrapper<T>::reset() {
+    BOOST_LOCK(mx);
+    vec.swap(new std::vector<T>());
+}
+
+template<class T>
+void async_containers::async_vector_wrapper<T>::resize(size_t new_size) {
+    BOOST_LOCK(mx);
+    vec.resize(new_size);
+}
+
+template<class T>
+bool async_containers::async_vector_wrapper<T>::write_to(size_t idx, T val, boost::system::error_code &err) {
+    BOOST_LOCK(mx);
+    using namespace boost::system;
+    if (idx > (vec.size() - 1)) {
+        err = errc::make_error_code(errc::result_out_of_range);
+        return false;
+    } else {
+        vec[idx] = val;
+        return true;
+    }
+}
+
+template<class T>
+bool async_containers::async_vector_wrapper<T>::get_from(size_t idx, T &res_storage,
+                                                         boost::system::error_code &err) {
+    BOOST_LOCK(mx);
+    if (idx > vec.size() - 1) {
+        err = boost::system::errc::make_error_code(boost::system::errc::result_out_of_range);
+        return false;
+    } else {
+        res_storage = vec.at(idx);
+        return true;
+    }
+}
+
+template<class T>
+bool async_containers::async_vector_wrapper<T>::put_data_from(std::vector<T> *location, size_t size,
+                                                              boost::system::error_code &err) {
+    BOOST_LOCK(mx);
+    using namespace boost::system;
+    if ((size > vec.size() - 1) || (size > location->size() - 1)) {
+        err = errc::make_error_code(errc::result_out_of_range);
+        return false;
+    } else {
+        memcpy(vec.data(), location->data(), size);
+        return true;
+    }
+}
+
+template<class T>
+bool async_containers::async_vector_wrapper<T>::put_data_to(std::vector<T> *location, size_t size,
+                                                            boost::system::error_code &err) {
+    using namespace boost::system;
+    BOOST_LOCK(mx);
+    if ((size > vec.size() - 1) || (size > location->size() - 1)) {
+        err = errc::make_error_code(errc::result_out_of_range);
+        return false;
+    } else {
+        memcpy(location->data(), vec.data(), size);
+        return true;
+    }
+}
+
+template<class T>
+bool async_containers::async_vector_wrapper<T>::copy_data_to_async_vec(async_vector_wrapper<T> *other_vec,
+                                                                       size_t size, boost::system::error_code &err) {
+    using namespace boost::system;
+    BOOST_LOCK(mx);
+    size_t other_size = 0;
+    other_vec->get_size(other_size);
+    if ((size > other_size - 1) || (size > vec.size() - 1)) {
+        err = errc::make_error_code(errc::result_out_of_range);
+        return false;
+    } else {
+        async_vector_wrapper::lock(other_vec);
+        memcpy(other_vec->get_data_ptr(), vec.data(), size);
+        async_vector_wrapper::unlock(other_vec);
+        return true;
+    }
+}
+
+template<class T>
+bool async_containers::async_vector_wrapper<T>::copy_data_from_async_vec(async_vector_wrapper<T> *other_vec,
+                                                                         size_t size, boost::system::error_code &err) {
+    using namespace boost::system;
+    BOOST_LOCK(mx);
+    size_t other_size = 0;
+    other_vec->get_size(other_size);
+    if ((size > other_size - 1) || (size > vec.size() - 1)) {
+        err = errc::make_error_code(errc::result_out_of_range);
+        return false;
+    } else {
+        async_vector_wrapper::lock(other_vec);
+        memcpy(vec.data(), other_vec->get_data_ptr(), size);
+        async_vector_wrapper::unlock(other_vec);
+        return true;
+    }
+}
---
+++ service.h
@@ -0,0 +1,91 @@
+#ifndef SERVICE_H
+#define SERVICE_H
+
+#include <boost/thread/thread.hpp>
+#include <boost/system/error_code.hpp>
+
+#include "time.h"
+#include <vector>
+
+//Макрос для заворачивания lock_guard
+#define BOOST_LOCK(mx) boost::lock_guard<boost::mutex> lock(mx)
+
+//cout функции для отладки (deprecated)
+#define DEBUG std::cout << "[DEBUG] "
+#define ERROR std::cout << "[ERROR] "
+
+// Обёртка над стандартным вектором для использования в асинхронных операциях
+namespace async_containers {
+    template<class storage_t>
+    class async_vector_wrapper {
+    private:
+        std::vector<storage_t> vec;
+        mutable boost::mutex mx;
+
+    public:
+        async_vector_wrapper();
+
+        // метод для блокирования мьютекса
+        static void lock(async_vector_wrapper<storage_t> &inst);
+
+        // метод для разблокирования мьютекса
+        static void unlock(async_vector_wrapper<storage_t> &inst);
+
+        storage_t *get_data_ptr(void);
+
+        //Получение размера хранимого вектора
+        void get_size(size_t &out_s);
+
+        // Очистка содержимого вектора
+        void clear(void);
+
+        // Полная очистка вектора (со сбросом всех параметров)
+        void reset(void);
+
+        // Изменение размера вектора
+        void resize(size_t new_size);
+
+        //Обёртка над записью по индексу
+        bool write_to(size_t idx, storage_t val, boost::system::error_code &err);
+
+        // Получение значения по индексу (с отслеживанием ошибок выполнения)
+        bool get_from(size_t idx,
+                      storage_t &res_storage,
+                      boost::system::error_code &err);
+
+        //Простое копирование в асинхронный объект из синхронного
+        bool put_data_from(std::vector<storage_t> *location,
+                           size_t size,
+                           boost::system::error_code &err);
+
+        // Копирование данных из объекта в другое синхронное расположение
+        bool put_data_to(std::vector<storage_t> *location,
+                         size_t size,
+                         boost::system::error_code &err);
+
+        // Копирование данных в другой асинхронный вектор
+        bool copy_data_to_async_vec(async_vector_wrapper<storage_t> *other_vec,
+                                    size_t size,
+                                    boost::system::error_code &err);
+
+        // Копирование данных из другого асинхронного вектора
+        bool copy_data_from_async_vec(async_vector_wrapper<storage_t> *other_vec,
+                                      size_t size,
+                                      boost::system::error_code &err);
+    };
+}
+
+//Функции преобразования времени
+namespace custom_time {
+    const time_t y06011998 = 315954000;
+
+    time_t local_time_to_UTC(time_t local_t);
+
+    time_t from_UTC_to_ISS(time_t utc_time);
+
+    time_t from_ISS_to_UTC(time_t uss_time);
+
+    time_t get_offset_from_zero(void);
+}
+
+#endif // SERVICE_H
